2c3123c6af917b3eb67371ae9a6003940d56fcbd
edwinvanderwal <edwin.vdwal@gmail.com>
2025-02-22 16:35:15 +0100

    Use right config.

--- src/main/java/com/edwinvanderwal/filewatcher/config/LedBoardConfig.java
+++ src/main/java/com/edwinvanderwal/filewatcher/config/LedBoardConfig.java
@@ -1,17 +1,8 @@
 package com.edwinvanderwal.filewatcher.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.PrintWriter;
-import java.net.Socket;
-
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.ip.tcp.TcpSendingMessageHandler;
-import org.springframework.integration.ip.tcp.connection.TcpNetClientConnectionFactory;
-import org.springframework.integration.ip.tcp.serializer.ByteArrayLfSerializer;
-
 import lombok.Getter;
 
 @Configuration
