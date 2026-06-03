86841b1955d07883cec80a1061bcca8b6412c61e
edwinvanderwal <edwin.vdwal@gmail.com>
2026-06-03 19:11:47 +0200

    Unit tests.

--- src/test/java/com/edwinvanderwal/filewatcher/service/DeelnemerServiceTest.java
+++ src/test/java/com/edwinvanderwal/filewatcher/service/DeelnemerServiceTest.java
@@ -1,12 +1,37 @@
 package com.edwinvanderwal.filewatcher.service;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.apache.commons.codec.binary.Hex;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.edwinvanderwal.filewatcher.model.Deelnemer;
+import com.edwinvanderwal.filewatcher.repository.DeelnemerRepo;
+
+@ExtendWith(MockitoExtension.class)
 class DeelnemerServiceTest {
-    
+
+    @Mock
+    private DeelnemerRepo deelnemerRepo;
+
+    @InjectMocks
+    private DeelnemerService deelnemerService;
+
+    @Test
+    void testSaveDeelnemer() {
+        Deelnemer deelnemer = new Deelnemer();
+        deelnemer.setReferentie(12343L);
+        deelnemerService.save(deelnemer);
+        verify(deelnemerRepo, times(1)).save(deelnemer);
+    }
+
+    // Below test don't contribute to the coverage, but are used to test the checksum calculation. The checksum is calculated by summing all bytes and then applying a bitwise AND with 0x7F to get the final checksum value. The tests verify that the calculated checksum matches the expected values for different byte arrays.
     @Test
     void testChecksum() {
         byte[] byteArray = {(byte)0x1b,(byte)0x40,(byte)0x53,(byte)0x5a,(byte)0x00,(byte)0x30,(byte)0x00,(byte)0x00,(byte)0x02,(byte)0x4d,(byte)0x49,
